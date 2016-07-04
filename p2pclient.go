90eeaeaad2aaf0eb42774dbe5093f651bf963df7
anbaoyong <anbaoyong@sina.cn>
2016-07-04 14:52:18 +0800

    规范代码格式

--- p2pclient.go
+++ p2pclient.go
@@ -7,17 +7,19 @@ cut-dirs默认是2，且不能修改
 */
 
 package main
+
 import (
-  "github.com/gin-gonic/gin"
-  "github.com/parnurzeal/gorequest"
-  "fmt"
-  "os/exec"
-  "sync"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "github.com/parnurzeal/gorequest"
+    "os/exec"
+    "sync"
 )
-var ( 
-Url string
-Status string
-wg sync.WaitGroup
+
+var (
+    Url    string
+    Status string
+    wg     sync.WaitGroup
 )
 
 func main() {
@@ -25,38 +27,40 @@ func main() {
     go accept()
     wg.Wait()
     request := gorequest.New()
-    _, _,errs := request.Get(Url).End()
+    _, _, errs := request.Get(Url).End()
     if len(errs) != 0 {
         fmt.Println("访问master失败")
     }
 }
+
 //接收master请求
 func accept() {
-  router := gin.Default()
-  router.GET("/client",func(c *gin.Context){
-    src := c.Query("src")
-    master := c.Query("master")
-    srcpath := c.Query("srcpath")
-    dstpath := c.Query("dstpath")
-    localhost := c.Query("localhost")
-    port := c.Query("port")
-    go  wget(src,srcpath,dstpath,master,localhost,port)
-   c.String(200,"客户端返回")
+    router := gin.Default()
+    router.GET("/client", func(c *gin.Context) {
+        src := c.Query("src")
+        master := c.Query("master")
+        srcpath := c.Query("srcpath")
+        dstpath := c.Query("dstpath")
+        localhost := c.Query("localhost")
+        port := c.Query("port")
+        go wget(src, srcpath, dstpath, master, localhost, port)
+        c.String(200, "客户端返回")
     })
     router.Run(":12306")
 }
 
-func wget(src,srcpath,dstpath,master,localhost,port string) {
+func wget(src, srcpath, dstpath, master, localhost, port string) {
     defer wg.Done()
-    download := fmt.Sprintf("wget -m -r -nH -P %s --limit-rate=80m --cut-dirs=2 ftp://%s%s",dstpath,src,srcpath)
+    download := fmt.Sprintf("wget -m -r -nH -P %s --limit-rate=80m --cut-dirs=2 ftp://%s%s", dstpath, src, srcpath)
     fmt.Println(download)
-   _,err := exec.Command("sh","-c",download).CombinedOutput()
+    _, err := exec.Command("sh", "-c", download).CombinedOutput()
     if err != nil {
         Status = "false"
     } else {
         Status = "true"
     }
-    fmt.Println(src,master,dstpath,localhost)
-    Url = fmt.Sprintf("http://%s:%s/p2p?host=%s&status=%s&src=%s",master,port,localhost,Status,src)
+    fmt.Println(src, master, dstpath, localhost)
+    Url = fmt.Sprintf("http://%s:%s/p2p?host=%s&status=%s&src=%s", master, port, localhost, Status, src)
 
 }
+
--- p2pserver.go
+++ p2pserver.go
@@ -1,50 +1,55 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "fmt"
-  "strconv"
-  "github.com/parnurzeal/gorequest"
-  "io"
-  "strings"
-  "bufio"
-  "os"
-  "time"
-  "flag"
+    "bufio"
+    "flag"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "github.com/parnurzeal/gorequest"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "time"
 )
+
 //定义slice
 var (
-SuccList []string //成功列表
-FailList []string //失败列表
-DstList []string //目的列表 
-//p2p Config
-ListenPort string
-Port *string
-Master *string
-Srcpath *string
-Dstpath *string
-Filename *string
-Timeout *int
+    SuccList []string //成功列表
+    FailList []string //失败列表
+    DstList  []string //目的列表
+    //p2p Config
+    ListenPort string
+    Port       *string
+    Master     *string
+    Srcpath    *string
+    Dstpath    *string
+    Filename   *string
+    Timeout    *int
 )
-var succ_ch = make(chan string,10000)
+var succ_ch = make(chan string, 10000)
+
 //读取文件，返回行 列表
 func Cat(filename string) []string {
-   f,err := os.Open(filename) 
-   defer f.Close()
-   if err != nil {
+    f, err := os.Open(filename)
+    defer f.Close()
+    if err != nil {
         panic(err)
-   } 
-   reader := bufio.NewReader(f)
-   for {
-        line,err := reader.ReadString('\n')
-        if err == io.EOF { break }
+    }
+    reader := bufio.NewReader(f)
+    for {
+        line, err := reader.ReadString('\n')
+        if err == io.EOF {
+            break
+        }
         line = strings.TrimSpace(line)
         if line != "" {
-            DstList = append(DstList,line)
+            DstList = append(DstList, line)
         }
-   }
-   return DstList
+    }
+    return DstList
 }
+
 //读取配置文件
 /*
 func Conf() *Config {
@@ -57,90 +62,94 @@ func Conf() *Config {
 */
 //接收客户端返回的信息
 func accept() {
-  router := gin.Default()
-  router.GET("/p2p",func(c *gin.Context){
-  status,_ := strconv.ParseBool(c.Query("status")) //判断客户端是否下载成功
-  host := c.Query("host")
-  src := c.Query("src") //接受客户端返回的数据源
-  if status {
-      succ_ch <- host
-      succ_ch <- src
-      SuccList = append(SuccList,host)
-  } else {
-      fmt.Println("客户端下载失败：",host)
-      succ_ch <- src
-  }
-  })
-  router.Run(ListenPort)
+    router := gin.Default()
+    router.GET("/p2p", func(c *gin.Context) {
+        status, _ := strconv.ParseBool(c.Query("status")) //判断客户端是否下载成功
+        host := c.Query("host")
+        src := c.Query("src") //接受客户端返回的数据源
+        if status {
+            succ_ch <- host
+            succ_ch <- src
+            SuccList = append(SuccList, host)
+        } else {
+            fmt.Println("客户端下载失败：", host)
+            succ_ch <- src
+        }
+    })
+    router.Run(ListenPort)
 }
+
 //request向客户端发送下载任务
-func request(master,port,src,dst,srcpath,dstpath string) {
+func request(master, port, src, dst, srcpath, dstpath string) {
     req := gorequest.New()
-    url := fmt.Sprintf("http://%s:12306/client?port=%s&src=%s&srcpath=%s&dstpath=%s&master=%s&localhost=%s",dst,port,src,srcpath,dstpath,master,dst)
+    url := fmt.Sprintf("http://%s:12306/client?port=%s&src=%s&srcpath=%s&dstpath=%s&master=%s&localhost=%s", dst, port, src, srcpath, dstpath, master, dst)
     fmt.Println(url)
     //_, _, errs := req.Get(url).Timeout(10*time.Second).End() //设置url超时时间为10妙
     _, _, errs := req.Get(url).End() //设置url超时时间为10妙
     if len(errs) != 0 {
-        fmt.Println("请求客户端失败:",dst)
+        fmt.Println("请求客户端失败:", dst)
         succ_ch <- src
     }
 }
+
 //主程序执行分发控制
 func handle(dstlist []string) {
     index := 0
     lendst := len(dstlist)
     for {
         select {
-            case succhost := <- succ_ch:
-                if index < lendst {
-                    request(*Master,*Port,succhost,dstlist[index],*Srcpath,*Dstpath)
-                    index = index + 1
+        case succhost := <-succ_ch:
+            if index < lendst {
+                request(*Master, *Port, succhost, dstlist[index], *Srcpath, *Dstpath)
+                index = index + 1
+            } else {
+                time.Sleep(time.Duration(*Timeout) * time.Second)
+                if lendst != len(SuccList) {
+                    FailList = Set(dstlist, SuccList)
+                    fmt.Printf("成功机器数为%d，机器列表为，机器列表为%v\n", len(SuccList), SuccList)
+                    fmt.Printf("失败机器数为%d，机器列表为，机器列表为%v\n", len(FailList), FailList)
                 } else {
-                    time.Sleep(time.Duration(*Timeout)*time.Second)
-                    if lendst != len(SuccList) {
-                        FailList = Set(dstlist,SuccList)
-                        fmt.Printf("成功机器数为%d，机器列表为，机器列表为%v\n",len(SuccList),SuccList)
-                        fmt.Printf("失败机器数为%d，机器列表为，机器列表为%v\n",len(FailList),FailList)
-                    } else {
-                        fmt.Printf("全部成功，成功机器数为%d，机器列表为，机器列表为%v\n",len(SuccList),SuccList)
-                    }
-                    fmt.Println("==============执行完毕=================")
-                    os.Exit(0)
-                } 
+                    fmt.Printf("全部成功，成功机器数为%d，机器列表为，机器列表为%v\n", len(SuccList), SuccList)
+                }
+                fmt.Println("==============执行完毕=================")
+                os.Exit(0)
+            }
         }
-    } 
+    }
 }
+
 //初始化
 func init() {
-//解析command line参数
+    //解析command line参数
     flag.Usage = func() {
         fmt.Println("Usage: <-m host> [-f file] [-s srcpath] [-d dstpath] [-p port] [-t timeout]")
         flag.PrintDefaults()
     }
-    Filename = flag.String("f","ip.txt","File that contains the target machine")
-    Srcpath = flag.String("s","/home/xiaoju","Data source path")
-    Dstpath = flag.String("d","/home/xiaoju","Data destination path")
-    Port =  flag.String("p","12306","Listen port")
-    Master = flag.String("m","","ip or host name of the master")
-    Timeout = flag.Int("t",1800,"If the master does not receive the return value within the specified time , that the transmission fails")
+    Filename = flag.String("f", "ip.txt", "File that contains the target machine")
+    Srcpath = flag.String("s", "/home/xiaoju", "Data source path")
+    Dstpath = flag.String("d", "/home/xiaoju", "Data destination path")
+    Port = flag.String("p", "12306", "Listen port")
+    Master = flag.String("m", "", "ip or host name of the master")
+    Timeout = flag.Int("t", 1800, "If the master does not receive the return value within the specified time , that the transmission fails")
     flag.Parse()
-    ListenPort = fmt.Sprintf(":%s",*Port)
+    ListenPort = fmt.Sprintf(":%s", *Port)
     succ_ch <- *Master //将master加入channel
 }
+
 //两个slice取差集
 func Set(one, two []string) []string {
     x := []string{}
     if len(two) != 0 {
-    for _, v := range one {
-        for k, vv := range two {
-            if v == vv {
-                break
-            }
-            if k == len(two)-1 {
-                x = append(x, v)
+        for _, v := range one {
+            for k, vv := range two {
+                if v == vv {
+                    break
+                }
+                if k == len(two)-1 {
+                    x = append(x, v)
+                }
             }
         }
-    }
     } else {
         return one
     }
@@ -154,3 +163,4 @@ func main() {
     go accept()
     handle(Cat(*Filename))
 }
+
